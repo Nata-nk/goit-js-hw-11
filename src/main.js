527380f2a1912732b1dd88a880c6859f3569be21
Nata-nk <137524429+Nata-nk@users.noreply.github.com>
2025-01-28 10:49:53 +0200

    2

--- src/main.js
+++ src/main.js
@@ -24,11 +24,12 @@ const onSearchFormSubmit = event => {
         .then(date => {
         spanLoader.classList.remove('loader');
             if (date.hits.length === 0) {
-                iziToast.show({
+                iziToast.error({
         message: 'Sorry, there are no images matching your search query. Please try again!',
         color: '#ef4040',
         theme: 'dark',
-        position: 'topRight',
+                    position: 'topRight',
+        iconUrl: './img/bi_x-octagon.svg',
         maxWidth: 360,
         });   
 }
